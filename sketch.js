cf8adbcbfe446255318c4ed50fd77e6d161993ed
Arush8822 <90975676+Arush8822@users.noreply.github.com>
2021-10-16 11:46:52 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+seaImg = loadImage("sea.png");
+ship1Img = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(600,400);
+
+
+  
+}
+
+function draw() {
+ background("blue");
+
+
+
+}
